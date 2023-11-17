65a9bf7564c754b57d48cde64e79a8d5689e78dd
FRSanchezflavio <flaviosanchez213@gmail.com>
2023-11-17 16:49:56 -0300

    cambiosfinalesrealizados

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,25 +1,38 @@
 /* eslint-disable react/no-unescaped-entities */
 import styles from './home.module.css';
-import cvPdf from '../../assets/MatiasSanchezCV.pdf'; 
+import cvPdf from '../../assets/MatiasSanchezCV.pdf';
 
 const Home = () => {
-    return (
-        <section className={styles.home} id='home'>
-            <div className={styles.introContent}>
-                <span className={styles.hello}>Hola,</span>
-                <span className={styles.introText}>Soy<span className={styles.introName}> Matias </span> <br />Desarrollador Web Full Stack </span>
-                <p className={styles.introPara}>Soy un desarrollador Full Stack con experiencia en la creacion de sitios y aplicaciones web completas <br /> el desarrollo y administración del backend</p>
-                <div className={styles.containerBtn}>
-                <a href="mailto:matiasanchez999@gmail.com" className={styles.contactBtn}>
-                Contact Me
-                </a>
-                <a href={cvPdf} className={styles.contactBtn} download>
-                CV
-                </a>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className={styles.home} id="home">
+      <div className={styles.introContent}>
+        <span className={styles.hello}>Hola,</span>
+        <span className={styles.introText}>
+          Somos<span className={styles.introName}> Matias y Flavio </span>{' '}
+          <br />
+          Desarrolladores Web Full Stack{' '}
+        </span>
+        <p className={styles.introPara}>
+          con experiencia en la creacion de sitios y aplicaciones web completas
+          y <br /> el desarrollo y administración del backend pueden ayudar a
+          desarrollar aplicaciones web y móviles <br />
+          más rápidamente y Mejorar la calidad de las aplicaciones web y
+          móviles.
+        </p>
+        <div className={styles.containerBtn}>
+          <a
+            href="mailto:matiasanchez999@gmail.com"
+            className={styles.contactBtn}
+          >
+            Contact Me
+          </a>
+          <a href={cvPdf} className={styles.contactBtn} download>
+            CV
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
